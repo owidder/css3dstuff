e93d04c36869e339ea906584795d86fa3b4d3e2b
Oliver Widder <oliver.widder@gmail.com>
2017-10-06 17:56:19 +0200

    use classes

--- react/cssd3/src/App.js
+++ react/cssd3/src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 import Bar3d from './Bar3d';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+    }
+
     componentDidMount() {
-        new Bar3d("#area", 100, 200);
+        new Bar3d("#area", 50, 250);
     }
     render() {
+        const style = {
+            position: 'absolute',
+            top: this.state.height/2,
+            width: this.state.width/2
+        };
         return (
-            <div id="area"></div>
+            <div style={style} id="area"></div>
         );
     }
 }
