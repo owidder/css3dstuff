15c4a1167c9f64705252e90e4a37d79bda9d2e2d
Oliver Widder <oliver.widder@gmail.com>
2017-10-07 17:00:50 +0200

    with rotation

--- react/cssd3/src/App.js
+++ react/cssd3/src/App.js
@@ -3,32 +3,35 @@ import './App.css';
 import drawBars from './drawBars';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     componentDidMount() {
         const data = [
             {
-                id: 0,
+                id: "1",
                 x: 100,
                 y: 50,
+                z: 100,
                 height: 400,
-                width: 200
+                width: 200,
+                rotationY: 10
             },
             {
-                id: 1,
+                id: "2",
                 x: 400,
                 y: 50,
+                z: 200,
                 height: 300,
-                width: 50
+                width: 50,
+                rotationY: 20
             },
             {
-                id: 2,
+                id: "3",
                 x: 600,
                 y: 50,
+                z: 300,
                 height: 150,
-                width: 60
+                width: 60,
+                rotationY: 30
             },
         ];
 
--- react/cssd3/src/drawBars.js
+++ react/cssd3/src/drawBars.js
@@ -1,6 +1,7 @@
 import * as d3 from 'd3';
+import * as _ from 'lodash';
 
-const drawBars = (rootSelector, data) => {
+const drawBars = (rootSelector, data, id) => {
 
     const root = d3.select(rootSelector);
 
@@ -12,40 +13,62 @@ const drawBars = (rootSelector, data) => {
         .attr("class", "container")
 
     enterSection.merge(selection)
-        .style("left", d => d.x + "px")
-        .style("top", d => d.y + "px")
+        .style("transform", d => "translateX(" + d.x + "px) translateY(" + d.y + "px) translateZ(" + d.z + "px) ")
         .style("height", d => d.height + "px")
         .style("width", d => d.width + "px")
 
+    const rotation = deg => "rotateX(-10deg) rotateY(" + deg + "deg)"
+
     const enterDiv = enterSection
         .append("div")
-        .attr("class", "cube spinning")
+        .attr("class", d => "cube _" + d.id)
+        .style("transform", rotation(10))
+        .on("click", d => {
+            d.rotationY += 90;
+            drawBars(rootSelector, data, d.id);
+        })
+
+    if(!_.isEmpty(id)) {
+        root.selectAll("div.cube._" + id)
+            .transition()
+            .duration(1000)
+            .styleTween("transform", d => d3.interpolate(rotation(d.rotationY-90), rotation(d.rotationY)))
+    }
 
     const topBottomPercentageHeight = d => (d.width / d.height * 100) + "%";
     const topBottomPercentageBottom = d => (d.width / d.height * 50) + "%";
 
     enterDiv
         .append("figure")
-        .attr("class", "front")
+        .attr("class", "front bar")
         .style("height", "100%")
+        .text("A")
     enterDiv
         .append("figure")
-        .attr("class", "back")
+        .attr("class", "back bar")
         .style("height", "100%")
+        .text("B")
     enterDiv
         .append("figure")
-        .attr("class", "right")
+        .attr("class", "right bar")
         .style("height", "100%")
+        .text("C")
     enterDiv
         .append("figure")
-        .attr("class", "left")
+        .attr("class", "left bar")
         .style("height", "100%")
+        .text("D")
     enterDiv
         .append("figure")
-        .attr("class", "top")
+        .attr("class", "top bar")
+        .text("E")
     enterDiv
         .append("figure")
-        .attr("class", "bottom")
+        .attr("class", "bottom bar")
+        .text("F")
+
+    root.selectAll("figure.bar")
+        .style("font-size", d => (d.height/5) + "px")
 
     root.selectAll("figure.front")
         .style("transform", d => "translateZ(" + (d.width/2) + "px)")
