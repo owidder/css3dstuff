4e427929498e7e0ebba9083d4777d2160df802a2
Oliver Widder <oliver.widder@gmail.com>
2017-10-07 11:33:05 +0200

    drawBars(3)

--- react/cssd3/src/App.js
+++ react/cssd3/src/App.js
@@ -5,31 +5,39 @@ import drawBars from './drawBars';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            width: window.innerWidth,
-            height: window.innerHeight
-        };
     }
 
     componentDidMount() {
-        const data = [{
-            id: 0,
-            x: 200,
-            y: 100,
-            height: 400,
-            width: 200
-        }];
+        const data = [
+            {
+                id: 0,
+                x: 100,
+                y: 50,
+                height: 400,
+                width: 200
+            },
+            {
+                id: 1,
+                x: 400,
+                y: 50,
+                height: 300,
+                width: 50
+            },
+            {
+                id: 2,
+                x: 600,
+                y: 50,
+                height: 150,
+                width: 60
+            },
+        ];
 
         drawBars("div#area", data)
     }
+
     render() {
-        const style = {
-            position: 'absolute',
-            top: this.state.height/2,
-            width: this.state.width/2
-        };
         return (
-            <div style={style} id="area"></div>
+            <div id="area"></div>
         );
     }
 }
